ccb71180f37addc7d6bc0f2b43a96a606c52ab70
Mukesh <msahu201989@gmail.com>
2022-12-14 21:10:21 +0530

    listners

--- locals.tf
+++ locals.tf
@@ -1,1 +1,4 @@
-locals {}
+locals {
+  DOCDB_USER = jsondecode(data.aws_secretsmanager_secret_version.roboshop.secret_string)["DOCDB_USER"]
+  DOCDB_PASS = jsondecode(data.aws_secretsmanager_secret_version.roboshop.secret_string)["DOCDB_PASS"]
+}
